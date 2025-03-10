680a2ffcc2d61a20bc4c964edd0a2788604c079f
kinire98 <iknitgarri@outlook.com>
2025-03-10 20:27:37 +0100

    feat: added test for game_won with some comments for later clarification

--- src-tauri/src/game/board/local_board/tests/game_won.rs
+++ src-tauri/src/game/board/local_board/tests/game_won.rs
@@ -1,1 +1,38 @@
+use crate::game::{
+    board::{Board, local_board::LocalBoard},
+    queue::local_queue::LocalQueue,
+};
 
+#[test]
+fn game_won_normal_game() {
+    let board = LocalBoard::new(LocalQueue::default());
+    assert!(!board.game_won(|_game_over, _lines| { false }));
+}
+
+#[test]
+fn game_won_test_40_lines_not_won_yet() {
+    let mut board = LocalBoard::new(LocalQueue::default());
+    board.lines_cleared = 39;
+    assert!(!board.game_won(|game_over, lines| { !game_over && lines >= 40 }))
+}
+
+#[test]
+fn game_won_test_40_lines_won() {
+    let mut board = LocalBoard::new(LocalQueue::default());
+    board.lines_cleared = 41;
+    assert!(board.game_won(|game_over, lines| { !game_over && lines >= 40 }))
+}
+
+#[test]
+fn game_won_2_minutes_not_passed() {
+    let board = LocalBoard::new(LocalQueue::default());
+    let seconds = 119;
+    assert!(!board.game_won(|game_over, _lines| { !game_over && seconds >= 120 }));
+}
+
+#[test]
+fn game_won_2_minutes_passed() {
+    let board = LocalBoard::new(LocalQueue::default());
+    let seconds = 120;
+    assert!(board.game_won(|game_over, _lines| { !game_over && seconds >= 120 }));
+}
--- src-tauri/src/game/board/mod.rs
+++ src-tauri/src/game/board/mod.rs
@@ -7,6 +7,8 @@ pub mod local_board;
 pub trait Board {
     /// Assume it's only be called when a piece is set
     fn game_over(&self) -> bool;
+    /// The win condition receives if the game has been meet one of the game over conditions
+    /// or to check the number of lines set
     fn game_won(&self, win_condition: impl Fn(bool, u32) -> bool) -> bool;
     fn board_state(&self) -> String;
     fn strategy(&self) -> Strategy;
