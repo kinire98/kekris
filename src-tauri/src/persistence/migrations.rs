9077f571b0c5143b67889a09ca1cda6187412447
kinire98 <iknitgarri@outlook.com>
2025-04-23 21:52:25 +0200

    fix

--- src-tauri/src/persistence/migrations.rs
+++ src-tauri/src/persistence/migrations.rs
@@ -2,16 +2,14 @@ use std::path::PathBuf;
 
 use sqlx::{SqlitePool, migrate::Migrator};
 use tauri::{AppHandle, Manager};
-use tokio::fs::{self, File};
+use tokio::fs;
 
 static MIGRATOR: Migrator = sqlx::migrate!();
 pub async fn run_migrations(app: AppHandle) {
     let base: PathBuf = app.path().app_data_dir().unwrap();
     let db_path = base.join("kekris.db");
     if !db_path.exists() {
-        fs::create_dir_all(base).await.unwrap();
-        // File::create(base).await.unwrap();
-        File::create(&db_path).await.unwrap();
+        fs::create_dir_all(&db_path).await.unwrap();
     }
     let url = format!("sqlite://{}", db_path.display());
 
